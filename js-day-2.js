b981c8d6c41670702c4071121b44a52663453489
learn <learn@learn06.local>
2019-06-04 15:53:11 -0700

    added new file

---
+++ js-day-2.js
@@ -0,0 +1,246 @@
+// // // // // // var someItem = 55;
+// // // // // // if (someItem<=100){
+// // // // // //     console.log("In budget.");
+// // // // // // }
+// // // // // // else{
+// // // // // //     console.log("Too much $$$.");
+// // // // // // }
+// // // // // // var hungry = false
+// // // // // // if (hungry){
+// // // // // //     console.log("eat food");
+// // // // // // }
+// // // // // // else {
+// // // // // //     console.log("keep coding");
+// // // // // // }
+// // // // // // var trafficLight = "yellow"
+// // // // // // if (trafficLight == "green") {
+// // // // // //     console.log("go")
+// // // // // // }
+// // // // // // else if (trafficLight == "yellow") {
+// // // // // //     console.log("slow down")
+// // // // // // // }
+// // // // // // // else {
+// // // // // // //     console.log("stop")
+// // // // // // // }
+// // // // // // // var numOne = 2
+// // // // // // // var numTwo = 9
+// // // // // // // if (numOne == numTwo) {
+// // // // // // //     console.log("is equal")
+// // // // // // // }
+// // // // // // // else if (numOne > numTwo) {
+// // // // // // //     console.log(numOne);
+// // // // // // // }
+// // // // // // // else {
+// // // // // // //     console.log(numTwo);
+// // // // // // // }
+// // // // // // // if (numOne == 0) {
+// // // // // // //     console.log("is zero")
+// // // // // // // }
+// // // // // // // else if(numOne%2 == 0) {
+// // // // // // //     console.log("is even");
+// // // // // // // }
+// // // // // // // else {
+// // // // // // //     console.log("is odd");
+// // // // // // // }
+// // // // // // // var grade = -5
+// // // // // // // if (grade >= 100) {
+// // // // // // //     console.log("perfect score");
+// // // // // // // }
+// // // // // // // else if(grade >= 90) {
+// // // // // // //     console.log("A");
+// // // // // // // }
+// // // // // // // else if(grade >= 80) {
+// // // // // // //     console.log("B");
+// // // // // // // }
+// // // // // // // else if (grade >= 70) {
+// // // // // // //     console.log("C");
+// // // // // // // }
+// // // // // // // else if (grade >= 60) {
+// // // // // // //     console.log("D");
+// // // // // // // }
+// // // // // // // else if (grade == 0) {
+// // // // // // //     console.log("no grade available");
+// // // // // // // }
+// // // // // // // else {
+// // // // // // //     console.log("F");
+// // // // // // // }
+// // // // // // // grade = "hello"
+// // // // // // // if (typeof grade == "number") {
+// // // // // // //     console.log("number");
+// // // // // // // }
+// // // // // // // else if(typeof grade == "string") {
+// // // // // // //     console.log("string");
+// // // // // // // }
+// // // // // // // else if(typeof grade == "boolean") {
+// // // // // // //     console.log("boolean");
+// // // // // // // }
+// // // // // // // else if (typeof grade == "symbol") {
+// // // // // // //     console.log("symbol");
+// // // // // // // }
+// // // // // // // else if (typeof grade == "undefined") {
+// // // // // // //     console.log("undefined");
+// // // // // // // }
+// // // // // // // else if (typeof grade == "null") {
+// // // // // // //     console.log("null");
+// // // // // // // }
+// // // // // // // else {
+// // // // // // //     console.log("F");
+// // // // // // // }
+// // // // // // //
+// // // // // // //
+// // // // // //
+// // // // // // function greeting(name) {
+// // // // // //     console.log('hello ' +name)
+// // // // // // }
+// // // // // // greeting('Bob')
+// // // // // // function marco(){
+// // // // // //     return 'polo'
+// // // // // // }
+// // // // // // console.log(marco())
+// // // // // // function oddOrEven(num){
+// // // // // //     if (num%2 == 0) {
+// // // // // //         return 'even'
+// // // // // //     } else {
+// // // // // //         return 'odd'
+// // // // // //     }
+// // // // // // }
+// // // // // // console.log(oddOrEven(3));
+// // // // // // function triple(num){
+// // // // // //     return num*3
+// // // // // // }
+// // // // // // console.log(triple(3));
+// // // // // // function multiply(num1, num2){
+// // // // // //     return num1 * num2
+// // // // // // }
+// // // // // // console.log(multiply(2,7));
+// // // // // // function divisibleBy(num1, num2){
+// // // // // //     if (num1%num2 === 0) {
+// // // // // //         return num1 + ' is evenly divisible by ' + num2
+// // // // // //     } else {
+// // // // // //         return 'It is not devisible by ' +num2
+// // // // // //     }
+// // // // // // }
+// // // // // // console.log(divisibleBy(10,3));
+// // // // //
+// // // // // //
+// // // // // // function greaterNum(num1, num2){
+// // // // // //     if (num1 >= num2) {
+// // // // // //         return num1
+// // // // // //     } else {
+// // // // // //         return num2
+// // // // // //     }
+// // // // // // }
+// // // // // // console.log(greaterNum(4,14));
+// // // // // //
+// // // // // // function helloWorld(lang){
+// // // // // //     if (lang === "es"){
+// // // // // //         return "Hola mundo"
+// // // // // //     } else if (lang === "de"){
+// // // // // //         return "Hellow world in German"
+// // // // // //     } else {
+// // // // // //         return "Hello World"
+// // // // // //     }
+// // // // // // }
+// // // // // // console.log(helloWorld("chinese"))
+// // // // // //
+// // // // //
+// // // // // // function assignGrade(score){
+// // // // // //     if (score >= 100){
+// // // // // //         return "perfect score"
+// // // // // //     } else if (score > 90){
+// // // // // //         return "A"
+// // // // // //     } else if (score > 80) {
+// // // // // //         return "B"
+// // // // // //     } else if (score > 70) {
+// // // // // //         return "C"
+// // // // // //     } else if (score > 60) {
+// // // // // //         return "D"
+// // // // // //     } else {
+// // // // // //         return "F"
+// // // // // //     }
+// // // // // // }
+// // // // // // console.log(assignGrade(1000));
+// // // // // //
+// // // // // //
+// // // // // //
+// // // // // // function pluralize(num, noun){
+// // // // // //     if (num >1) {
+// // // // // //         if (noun === "sheep"){}
+// // // // // //         else if(noun === "child"){
+// // // // // //             noun = "children"
+// // // // // //         } else{
+// // // // // //             noun=noun.concat("s")
+// // // // // //         }
+// // // // // //     }
+// // // // // //     return num +" "+ noun
+// // // // // // }
+// // // // // // console.log(pluralize(50, "child"));
+// // // // //
+// // // // //
+// // // // // // var array1 = [1,2,3]
+// // // // // // var array2 = [4,5,6]
+// // // // // // var threeDArray = [array1, array2]
+// // // // // // console.log(threeDArray);
+// // // // // //
+// // // // // // var groceryList2 = ["chips", "dip", "cookies"]
+// // // // // // groceryList2.push("soda")
+// // // // // // console.log(groceryList2);
+// // // // // // var numbers2 = [2,4,6,8,10]
+// // // // // // numbers2.unshift(0)
+// // // // // // console.log(numbers2);
+// // // // // // var groceryList1 = ["apples", "carrots", "oatmeal"]
+// // // // // // var newList = groceryList1.concat("granola")
+// // // // // // console.log(newList);
+// // // // // // console.log(groceryList1);
+// // // // // // var numbers1 = [1,2,3,4,5]
+// // // // // // var newNumbers = [0].concat(numbers1)
+// // // // // // console.log(newNumbers);
+// // // // // // var numbers3 = [2,13,6,8,4,2]
+// // // // // // console.log(numbers3.indexOf(2));
+// // // // // // console.log(numbers3.lastIndexOf(2));
+// // // // // // var chars = ['y','a','r','r','a']
+// // // // // // var reverseChars = chars.reverse().join('')
+// // // // // // var chars2 = chars
+// // // // // // console.log(reverseChars);
+// // // // // // var sibs = ['Bob', 'Sally', 'Joe']
+// // // // // // var parents = ['Mom', 'Dad']
+// // // // // // console.log(sibs.sort());
+// // // // // // console.log(parents.sort().reverse())
+// // // // // // var combine = sibs.concat(parents).sort().reverse()
+// // // // // // console.log(combine);
+// // // // // // var ball = ['Yup!', 'Fuhgeddaboutid','maybe', 'ask: what would your mother do?', 'Ask: what would an Australian do, then do the opposite', 'roll again']
+// // // // // // var ranNum = Math.floor(Math.random()*6)
+// // // // // // var questions = ball[ranNum]
+// // // // // // // console.log(questions);
+// // // // // // function myFunc() {
+// // // // // //     var ranNum = Math.floor(Math.random()*6)
+// // // // // //     var questions = ball[ranNum]
+// // // // // //     document.getElementById('h1').innerHTML = questions
+// // // // // // }
+// // // // // // console.log(i);
+// // // // //
+// // // // // for (var i=1; i<21; i++){
+// // // // //     console.log(i*3);
+// // // // // }
+// // // //
+// // // // var numbers = [8,2,17,4,5,10,4,8,9,15]
+// // // // for (let i = 0; i<numbers.length; i++){
+// // // //     console.log(numbers[i]*4);
+// // // // }
+// // // for (let i = 1; i<21; i++){
+// // //     if (i%2 === 1){
+// // //         console.log("ODD");
+// // //     } else {
+// // //         console.log(i);
+// // //     }
+// // // }
+// // for (let i = 10; i > 0; i--){
+// //     // console.log(i);
+// var letters = ["l", "e", "a", "r", "n"]
+// for (let i = 0; i < letters.length; i++){
+//     console.log(letters[i].toUpperCase());
+// }
+var strings = ["hi", "yo", "there", "what", "how", "two", "are", "where", "you"]
+for (let i = 0; i < strings.length; i+=2){
+    console.log(strings[i]);
+}
